3901a22da6e71ed3869432a87ea54b030942f68b
Dujardin Nolwenn <nolwenn.dujardin@etudiant.univ-rennes1.fr>
2024-01-09 19:44:57 +0100

    Ajout test sort
    Bubble sort à revoir, boucle infinie

--- code/sorting/src/test/java/fr/istic/vv/SortingTest.java
+++ code/sorting/src/test/java/fr/istic/vv/SortingTest.java
@@ -1,9 +1,42 @@
 package fr.istic.vv;
+
 import net.jqwik.api.*;
+import net.jqwik.api.constraints.Size;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class SortingTest {
     @Property
-    boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
-        return Math.abs(anInteger) >= 0;
+    void testBubbleSort(@ForAll @Size(min=1, max=100) Integer[] array) {
+        Comparator<Integer> comparator = Integer::compare;
+
+        Integer[] originalArray = Arrays.copyOf(array, array.length);
+        Arrays.sort(originalArray, comparator);
+
+        assertArrayEquals(originalArray, Sorting.bubblesort(array,comparator));
     }
+
+    @Property
+    void testMergeSort(@ForAll @Size(min=1, max=100) Integer[] array) {
+        Comparator<Integer> comparator = Integer::compare;
+
+        Integer[] originalArray = Arrays.copyOf(array, array.length);
+        Arrays.sort(originalArray, comparator);
+
+        assertArrayEquals(originalArray, Sorting.mergesort(array,comparator));
+    }
+
+    @Property
+    void testQuickSort(@ForAll @Size(min=1, max=100) Integer[] array) {
+        Comparator<Integer> comparator = Integer::compare;
+
+        Integer[] originalArray = Arrays.copyOf(array, array.length);
+        Arrays.sort(originalArray, comparator);
+
+        assertArrayEquals(originalArray, Sorting.quicksort(array,comparator));
+    }
+
 }
