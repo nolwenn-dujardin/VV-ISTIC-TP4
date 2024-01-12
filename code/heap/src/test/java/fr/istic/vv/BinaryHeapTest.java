0da1ea7d3fec320e902bd9d3c333e29e1fe41c99
Dujardin Nolwenn <nolwenn.dujardin@etudiant.univ-rennes1.fr>
2024-01-12 16:04:05 +0100

    Implémentation BinaryHeap

--- code/heap/src/test/java/fr/istic/vv/BinaryHeapTest.java
+++ code/heap/src/test/java/fr/istic/vv/BinaryHeapTest.java
@@ -7,4 +7,10 @@ public class BinaryHeapTest {
     boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
         return Math.abs(anInteger) >= 0;
     }
+
+    /**
+     * Generate random inputs for the heap and ensures the element returned
+     * by pop every time it is invoked is the minimum of the remaning elements
+     * in the heap
+     */
 }
